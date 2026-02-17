450217561aae95f3492e45a013c8aa6fa63955ce
juanjosefg2024 <juanjose.fernandez@ext.diggia.com>
2026-02-17 09:10:03 +0100

    Add files via upload
    Versión inicial de la estimación de la posición del nodo

---
+++ simulacion_basica_rssi_v1.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def posicionamiento (rssi_n,escenario):
+
+    aux=[]
+    x_vecino=[]
+    y_vecino=[]
+    rssi_vecino=[]
+    var=[]
+
+    for i in range(len(escenario['x'])):
+        rssia = escenario['RSSIA'][i]
+        rssib = escenario['RSSIB'][i]
+        rssic = escenario['RSSIC'][i]
+        var = np.sqrt((rssi_n[0] - rssia)**2 + (rssi_n[1] - rssib)**2 + (rssi_n[2] - rssic)**2)
+        aux.append(round(float(var), 2))
+
+    indices = np.argsort(aux)[:1] # indices del valores minimos
+    #indices = np.argsort(aux)[:3] # Indices de los 3 valores minimos (3 vecinos)
+
+    
+    for j in indices:
+        x_vecino.append(escenario['x'][j])
+        y_vecino.append(escenario['y'][j])
+        rssi_vecino.append([escenario['RSSIA'][j],escenario['RSSIB'][j],escenario['RSSIC'][j]])
+    return x_vecino,y_vecino,rssi_vecino
+
+def run_simulacion():
+    archivo = '~/Escritorio/escenario.xlsx'
+    df = pd.read_excel(archivo)
+    escenario = df.to_dict(orient='list')
+
+    pos_beacons = [(0,0),(0,4),(4,0)]
+    x_beacons=[b[0] for b in pos_beacons]
+    y_beacons=[b[1] for b in pos_beacons]
+
+# Valores RSSI del nodo. Como ejemplo he usado 
+    rssi_n=[
+        [-53.51544993,-60.50514998,-60.50514998],
+        [-60.50514998,-53.51544993,-61.96643034],
+        [-60,-55,-61],
+        [-58.2,-60.5,-60],
+        [-55,-60.7,-61],
+        [-60.07421924,-59,-60.07421924],
+        [-60.50514998,-61.96643034,-53.51544993],
+        [-61.96643034,-60.50514998,-60.50514998]
+    ]
+    num_puntos=len(rssi_n)
+
+    plt.figure(figsize=(10,10))
+    plt.scatter(x_beacons, y_beacons, color='red', marker='^') # Beacons
+    plt.scatter(escenario['x'], escenario['y'], color='green') # Puntos del footprint
+    for i in range(num_puntos):
+        Xi,Yi,RSSI = posicionamiento (rssi_n[i],escenario) # Posicionamiento estimado
+        plt.scatter(Xi, Yi,color='red') # Posición estimada correspondiente a un punto del footprint
+        print(RSSI)
+
+    plt.title('Simulación de posicionamiento')
+    plt.xlabel('Coordenada X')
+    plt.ylabel('Coordenada Y')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+if __name__ == "__main__":
+    run_simulacion()
